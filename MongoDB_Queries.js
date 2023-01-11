6a24a11ef932ccbbaa59a231622d7080fd28150d
Joël Arana <joel.aranag@gmail.com>
2023-01-11 01:42:12 +0100

    correccio i afegit 21 22

--- MongoDB_Queries.js
+++ MongoDB_Queries.js
@@ -7,9 +7,9 @@ db.restaurants.find({"borough" : "Bronx"}).limit(5);
 db.restaurants.find({"borough" : "Bronx"}).skip(5).limit(5);
 db.restaurants.find({"grades.score" : {$gt:90}});
 db.restaurants.find({"grades.score" : {$gt:80, $lt:100}});
-db.restaurants.find({"address.coord" : {$lt : -95.754168}});
-db.restaurants.find({ $and: [ {"cuisine" : {$ne :"American "} }, {"grades.score" : {$gt : 70}} , {"address.coord" : {$lt : -65.754168}} ] });
-db.restaurants.find({"cuisine" : {$ne : "American "},"grades.score" :{$gt: 70}, "address.coord" : {$lt : -65.754168}});
+db.restaurants.find({"address.coord.1" : {$lt : -95.754168}});
+db.restaurants.find({ $and: [ {"cuisine" : {$ne :"American "} }, {"grades.score" : {$gt : 70}} , {"address.coord.1" : {$lt : -65.754168}} ] });
+db.restaurants.find({"cuisine" : {$ne : "American "},"grades.score" :{$gt: 70}, "address.coord.0" : {$lt : -65.754168}});
 db.restaurants.find({"cuisine" : {$ne : "American "},"grades.grade" : "A", "borough" : { $ne: "brooklyn"}}).sort({"cuisine" : -1});
 db.restaurants.find({"name": /^Wil/ }, {"restaurant_id" : 1, "name" : 1, "borough": 1, "cuisine": 1});
 db.restaurants.find({"name": /ces$/ }, {"restaurant_id" : 1, "name" : 1, "borough": 1, "cuisine": 1});
@@ -18,3 +18,5 @@ db.restaurants.find({"borough": "Bronx", $or : [{"cuisine": "American"},{"cuisin
 db.restaurants.find({"borough": {$in : ["Staten Island", "Queens", " Bronx", "Brooklyn"]}}, {"restaurant_id" : 1, "name" : 1, "borough": 1, "cuisine": 1});
 db.restaurants.find({"borough": {$nin : ["Staten Island", "Queens", " Bronx", "Brooklyn"]}}, {"restaurant_id" : 1, "name" : 1, "borough": 1, "cuisine": 1});
 db.restaurants.find({"grades.score" :{$lt: 10} }, {"restaurant_id" : 1, "name" : 1, "borough": 1, "cuisine": 1});
+db.restaurants.find({$or: [{name: /^Wil/}, {"$and": [{"cuisine" : {$ne :"American "}}, {"cuisine" : {$ne :"Chinees"}}]}]},{"restaurant_id" : 1,"name":1,"borough":1,"cuisine" :1});
+db.restaurants.find( {"grades.date": ISODate("2014-08-11T00:00:00Z"), "grades.grade":"A" , "grades.score" : 11}, {"restaurant_id" : 1,"name":1,"grades":1});
